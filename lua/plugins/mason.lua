0720f70b0e974f400de6ef3e34e78aafe99c3e48
kimsang <boh001g@gmail.com>
2023-07-22 14:29:14 +0900

    feat: add keymap for goTodefinition

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -35,6 +35,61 @@ cmp.setup({
 })
 
 local lspconfig = require("lspconfig")
+local function lsp_definition_in_tab()
+  -- 현재 버퍼와 위치 저장
+  local current_bufnr = vim.api.nvim_get_current_buf()
+  local current_winid = vim.api.nvim_get_current_win()
+  local current_cursor = vim.api.nvim_win_get_cursor(current_winid)
+
+  -- 새 탭을 열고 해당 탭으로 이동
+  vim.api.nvim_command('tabnew')
+
+  -- 저장한 버퍼로 이동
+  vim.api.nvim_set_current_buf(current_bufnr)
+  vim.api.nvim_win_set_cursor(0, current_cursor)
+
+  -- LSP의 `vim.lsp.buf.definition()` 함수를 호출하여 정의로 이동
+  vim.lsp.buf.definition()
+end
+
+-- Global mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+
+-- Use LspAttach autocommand to only map the following keys
+-- after the language server attaches to the current buffer
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  callback = function(ev)
+    -- Enable completion triggered by <c-x><c-o>
+    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+    -- Buffer local mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local opts = { buffer = ev.buf }
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+    vim.keymap.set('n', 'ga', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'gd', lsp_definition_in_tab, opts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+    vim.keymap.set('n', '<space>wl', function()
+      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, opts)
+    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+    vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', '<space>f', function()
+      vim.lsp.buf.format { async = true }
+    end, opts)
+  end,
+})
 
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
