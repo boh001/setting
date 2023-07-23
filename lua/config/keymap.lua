866062be06108bc3501a805c2bc80430d667e5e3
kimsang <boh001g@gmail.com>
2023-07-23 23:36:36 +0900

    feat: add bracket function

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -1,14 +1,14 @@
 local keymap = vim.keymap.set
 local silent = { silent = true }
 
-function get_char_before_cursor()
+local get_char_before_cursor = function()
     local line, col = unpack(vim.api.nvim_win_get_cursor(0))
     local line_content = vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]
     local char_at_cursor = string.sub(line_content, col, col)
     return char_at_cursor
 end
 
-function get_char_after_cursor()
+local get_char_after_cursor = function ()
     local line, col = unpack(vim.api.nvim_win_get_cursor(0))
     col = col + 1
     local line_content = vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]
@@ -17,9 +17,9 @@ function get_char_after_cursor()
 end
 
 -- bracket and quote
-keymap("i", "{", "{}<Esc>ha", slient)
-keymap("i", "[", "[]<Esc>ha", slient)
-keymap("i", "(", "()<Esc>ha", slient)
+keymap("i", "{", "{}<Esc>ha", silent)
+keymap("i", "[", "[]<Esc>ha", silent)
+keymap("i", "(", "()<Esc>ha", silent)
 
 function is_bracket(input)
   return input == "{}" or input == "[]" or input == "()"
@@ -32,8 +32,10 @@ end
 local indent_in_bracket = function ()
   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
   local line_content = vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]
+  local content = string.sub(line_content, col, col + 1)
 
-  if is_bracket(line_content) then
+  print(content)
+  if is_bracket(content) then
     return "<CR><Esc>ko<Esc><S-i><Tab>"
   else
     return "<CR>"
@@ -71,4 +73,5 @@ keymap("i", "]", jump_bracket_to_next("]"), { silent = true, expr = true })
 keymap("i", '"', jump_quote_to_next('"'), { silent = true, expr = true })
 keymap("i", "'", jump_quote_to_next("'"), { silent = true, expr = true })
 keymap("i", "`", jump_quote_to_next("`"), { silent = true, expr = true })
+
 
