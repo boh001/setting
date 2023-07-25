252ca83798adf5c9f5596f2e1279d522176710fd
kimsang <boh001g@gmail.com>
2023-07-25 21:56:30 +0900

    feat: change keymap to delete current buffer

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -2,18 +2,18 @@ local keymap = vim.keymap.set
 local silent = { silent = true }
 
 local get_char_before_cursor = function()
-    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-    local line_content = vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]
-    local char_at_cursor = string.sub(line_content, col, col)
-    return char_at_cursor
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  local line_content = vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]
+  local char_at_cursor = string.sub(line_content, col, col)
+  return char_at_cursor
 end
 
-local get_char_after_cursor = function ()
-    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-    col = col + 1
-    local line_content = vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]
-    local char_after_cursor = string.sub(line_content, col, col)
-    return char_after_cursor
+local get_char_after_cursor = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  col = col + 1
+  local line_content = vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]
+  local char_after_cursor = string.sub(line_content, col, col)
+  return char_after_cursor
 end
 
 -- bracket and quote
@@ -29,7 +29,7 @@ function is_quote(input)
   return input == '""' or input == "''" or input == "``"
 end
 
-local indent_in_bracket = function ()
+local indent_in_bracket = function()
   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
   local line_content = vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]
   local content = string.sub(line_content, col, col + 1)
@@ -42,24 +42,24 @@ local indent_in_bracket = function ()
 end
 
 local jump_bracket_to_next = function(input)
-  return function ()
+  return function()
     local char_after_cursor = get_char_after_cursor()
 
     if char_after_cursor == input then
-      return "<Esc>la" 
-    else 
+      return "<Esc>la"
+    else
       return input
     end
   end
 end
 
 local jump_quote_to_next = function(input)
-  return function ()
+  return function()
     local char_after_cursor = get_char_after_cursor()
 
     if char_after_cursor == input then
-      return "<Esc>la" 
-    else 
+      return "<Esc>la"
+    else
       return input .. input .. "<Esc>ha"
     end
   end
@@ -73,4 +73,4 @@ keymap("i", '"', jump_quote_to_next('"'), { silent = true, expr = true })
 keymap("i", "'", jump_quote_to_next("'"), { silent = true, expr = true })
 keymap("i", "`", jump_quote_to_next("`"), { silent = true, expr = true })
 
-
+keymap("n", "-", ":bd<CR>", { silent = true })
