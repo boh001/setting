a7580951b976aafbea5cd235ebb9b3d87ddcdb95
kimsang <boh001g@gmail.com>
2023-07-27 21:04:24 +0900

    feat: add blankline

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -104,6 +104,6 @@ return {
   {
     'rmagatti/session-lens',
     dependencies = { 'rmagatti/auto-session', 'nvim-telescope/telescope.nvim' },
-  }
-
+  },
+  "lukas-reineke/indent-blankline.nvim",
 }
