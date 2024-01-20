412f593dbabb4a40a812335d8683f657b3c0e935
sanghyeon.kim <boh001g@gmail.com>
2024-01-20 18:23:37 +0900

    update

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -127,14 +127,4 @@ return {
 			"rcarriga/nvim-notify",
 		},
 	},
-	"jackMort/ChatGPT.nvim",
-	event = "VeryLazy",
-	config = function()
-		require("chatgpt").setup()
-	end,
-	dependencies = {
-		"MunifTanjim/nui.nvim",
-		"nvim-lua/plenary.nvim",
-		"nvim-telescope/telescope.nvim",
-	},
 }
--- lua/plugins/gitblame.lua
+++ lua/plugins/gitblame.lua
@@ -1,4 +1,7 @@
--- Lua 파일에서 g:gitblame_message_template 설정하기
+require('gitblame').setup {
+     --Note how the `gitblame_` prefix is omitted in `setup`
+    enabled = false,
+}
 vim.api.nvim_set_var('gitblame_message_template', '<committer>, <sha> (<committer-date>)')
 vim.api.nvim_set_var('gitblame_date_format', '%r')
 
