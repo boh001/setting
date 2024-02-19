d6ca7ff4e6ba4cd61985d2d43e5176f3dd8bfcaf
sanghyeon.kim <boh001g@gmail.com>
2024-02-20 01:34:18 +0900

    update

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,28 +1,28 @@
 require("toggleterm").setup({
-  open_mapping = [[<c-\>]],
-  direction = 'float',
-  hide_number = false,
+	open_mapping = [[<c-\>]],
+	direction = "float",
+	hide_number = false,
 })
 
 function _G.set_terminal_keymaps()
-  local opts = { noremap = true }
-  vim.api.nvim_buf_set_keymap(0, 't', '<C-]>', [[<C-\><C-n>]], opts)
-  vim.api.nvim_buf_set_keymap(0, 'n', '<C-]>', ':q<CR>', opts)
+	local opts = { noremap = true }
+	vim.api.nvim_buf_set_keymap(0, "t", "<C-]>", [[<C-\><C-n>]], opts)
+	vim.api.nvim_buf_set_keymap(0, "n", "<C-]>", ":q<CR>", opts)
 end
 
 -- if you only want these mappings for toggle term use term://*toggleterm#* instead
-vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
 
-local Terminal  = require('toggleterm.terminal').Terminal
+local Terminal = require("toggleterm.terminal").Terminal
 local lazygit = Terminal:new({ cmd = "lazygit", hidden = true, count = 6 })
 
 function _lazygit_toggle()
-  lazygit:toggle()
+	lazygit:toggle()
 end
 
-vim.keymap.set("n", "<leader>g", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true})
-vim.keymap.set("n", "<leader>1", ":1ToggleTerm<CR>", {noremap = true, silent = true})
-vim.keymap.set("n", "<leader>2", ":2ToggleTerm<CR>", {noremap = true, silent = true})
-vim.keymap.set("n", "<leader>3", ":3ToggleTerm<CR>", {noremap = true, silent = true})
-vim.keymap.set("n", "<leader>4", ":4ToggleTerm<CR>", {noremap = true, silent = true})
-vim.keymap.set("n", "<leader>5", ":5ToggleTerm<CR>", {noremap = true, silent = true})
+vim.keymap.set("n", "<leader>g", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>1", ":1ToggleTerm dir=%:p:h<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>2", ":2ToggleTerm dir=%:p:h<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>3", ":3ToggleTerm dir=%:p:h<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>4", ":4ToggleTerm dir=%:p:h<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>5", ":5ToggleTerm dir=%:p:h<CR>", { noremap = true, silent = true })
