d02c5e4a9e1c2e5d7832377c15e61d0c32a4e1ca
sanghyeon.kim <boh001g@gmail.com>
2024-02-26 22:29:39 +0900

    update

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -15,14 +15,20 @@ vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
 
 local Terminal = require("toggleterm.terminal").Terminal
 local lazygit = Terminal:new({ cmd = "lazygit", hidden = true, count = 6 })
+local lazydocker = Terminal:new({ cmd = "lazydocker", hidden = true, count = 7 })
 
 function _lazygit_toggle()
-	lazygit:toggle()
+  lazygit:toggle()
 end
 
-vim.keymap.set("n", "<leader>g", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
+function _lazydocker_toggle()
+  lazydocker:toggle()
+end
+
 vim.keymap.set("n", "<leader>1", ":1ToggleTerm<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>2", ":2ToggleTerm<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>3", ":3ToggleTerm<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>4", ":4ToggleTerm<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>5", ":5ToggleTerm<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>lg", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>ld", "<cmd>lua _lazydocker_toggle()<CR>", { noremap = true, silent = true })
